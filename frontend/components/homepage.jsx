45f80ebbf7856f56f5234b81016ea133ef79daa4
Jordan Phan <JordanJPhan@gmail.com>
2020-08-28 04:57:31 -0700

    update splash page

--- frontend/components/homepage.jsx
+++ frontend/components/homepage.jsx
@@ -7,12 +7,12 @@ class Homepage extends React.Component {
     render() {
         return (
             <div>
-                <div className="splash-image">
-                    <Link to='/sneakers/1' >
-                        <img src="https://images.unsplash.com/photo-1516478177764-9fe5bd7e9717?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80" height='800px' width='100%' />
-                    </Link>
-                    <h2 className="splash-text"> AIR JORDAN 1 - CHICAGO </h2>
-                </div>
+                <Link to='/sneakers/1' >
+                    <div className="splash-image">
+                            <img src="https://images.unsplash.com/photo-1516478177764-9fe5bd7e9717?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80" height='800px' width='100%' />
+                            <h2 className="splash-text"> AIR JORDAN 1 - CHICAGO </h2>
+                    </div>
+                </Link>
 
             </div>
         )
